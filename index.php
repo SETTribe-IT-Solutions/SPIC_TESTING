af8d70ce91341b6832c59d4cdf15c815e38b83ef
saurabhrapanwar <saurabhrapanwar@gmail.com>
2025-03-06 16:59:43 +0530

    saurabh changed new file

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ session_start();
 if (!isset($_SESSION['username'])) {
     header("Location: ./pages/login.php");
     exit();
+
+    //Saurabh has edited this file
 }
 if (isset($_SESSION['username'])) {
   header("Location: ./pages/dashboard.php");
